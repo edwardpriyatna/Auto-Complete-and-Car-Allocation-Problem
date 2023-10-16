a6d26dda064f5f85361b2af56b803f6940d80686
edwa0003 <edwa0003@student.monash.edu>
2023-10-16 11:38:54 +0800

    idk man

--- q2.py
+++ q2.py
@@ -1,209 +1,144 @@
-import math
-
 class Vertex:
     def __init__(self, name):
         self.name = name
-        self.visited = False
-        self.parent = None
+        self.edges = []  # list of edges connected to this vertex
 
     def __str__(self):
         return self.name
 
+
 class Edge:
-    def __init__(self, start_vertex, end_vertex, capacity, flow=0):
-        self.start_vertex = start_vertex
-        self.end_vertex = end_vertex
+    def __init__(self, source, target, capacity):
+        self.source = source
+        self.target = target
         self.capacity = capacity
-        self.flow = flow
+        self.flow = 0
 
     def __str__(self):
-        return f"{self.start_vertex} -> {self.end_vertex} : {self.flow}/{self.capacity}"
+        return f"{self.source} -> {self.target} | Capacity: {self.capacity} | Flow: {self.flow}"
 
-class NetworkFlow:
+
+class Network:
     def __init__(self):
         self.vertices = []
-        self.edges = []
 
-    def add_vertex(self, vertex):
+    def add_vertex(self, name):
+        vertex = Vertex(name)
         self.vertices.append(vertex)
+        return vertex
 
-    def add_edge(self, edge):
-        self.edges.append(edge)
+    def add_edge(self, source, target, capacity):
+        edge = Edge(source, target, capacity)
+        source.edges.append(edge)
+        return edge
 
     def __str__(self):
-        return "\n".join(str(edge) for edge in self.edges)
+        result = "Network:\n"
+        for v in self.vertices:
+            result += f"Vertex: {v}\n"
+            for e in v.edges:
+                result += f"  {e}\n"
+        return result
 
-    def make_network(self, preferences, licenses):
-        # Create vertices
-        source1 = Vertex('source1')
-        source2 = Vertex('source2')
-        sink = Vertex('sink')
-        p_vertices = [Vertex(f'p{i}') for i in range(len(preferences))]
-        d_vertices = [Vertex(f'd{i}') for i in range(math.ceil(len(preferences) / 5))]
-        c_vertices = [Vertex(f'c{i}') for i in range(math.ceil(len(preferences) / 5))]
 
-        # Add vertices to the network
-        for vertex in [source1, source2] + p_vertices + d_vertices + c_vertices + [sink]:
-            self.add_vertex(vertex)
-
-        # Add edge from source1 to source2
-        self.add_edge(Edge(source1, source2, len(preferences)))
-
-        # Add edges from source2 to p_vertices
-        for p_vertex in p_vertices:
-            self.add_edge(Edge(source2, p_vertex, 1))
-
-        # Add edges from licensed p_vertices to d_vertices based on preferences
-        for i, prefs in enumerate(preferences):
-            if i in licenses:
-                p_vertex = p_vertices[i]
-                for pref in prefs:
-                    d_vertex = d_vertices[pref]
-                    self.add_edge(Edge(p_vertex, d_vertex, 1))
-
-        # Add edges from d_vertices to sink
-        for d_vertex in d_vertices:
-            self.add_edge(Edge(d_vertex, sink, 2))
-
-        # Add edges from p_vertices to c_vertices based on preferences
-        for i, prefs in enumerate(preferences):
-            p_vertex = p_vertices[i]
-            for pref in prefs:
-                c_vertex = c_vertices[pref]
-                self.add_edge(Edge(p_vertex, c_vertex, 1))
-
-        # Add edges from c_vertices to sink
-        for c_vertex in c_vertices:
-            self.add_edge(Edge(c_vertex, sink, 3))
-
-class ResidualNetwork(NetworkFlow):
-    def __init__(self, network):
+class ResidualNetwork(Network):
+    def __init__(self):
         super().__init__()
-        self.vertices = network.vertices
-        self.edges = [Edge(edge.start_vertex, edge.end_vertex, edge.capacity, edge.flow) for edge in network.edges]
 
-    def has_AugmentingPath(self, source, sink):
-        queue = [source]
-        source.visited = True
+    def bfs(self, source, sink, parent):
+        visited = [False] * len(self.vertices)
+        queue = []
+        queue.append(source)
+        visited[self.vertices.index(source)] = True
 
         while queue:
-            vertex = queue.pop(0)
-            for edge in self.edges:
-                residual_capacity = edge.capacity - edge.flow
-                if edge.start_vertex == vertex and not edge.end_vertex.visited and residual_capacity > 0:
-                    edge.end_vertex.parent = edge  # Use the parent attribute of Vertex
-                    if edge.end_vertex == sink:
-                        self._reset_visited()
-                        return True
-                    queue.append(edge.end_vertex)
-                    edge.end_vertex.visited = True
-        self._reset_visited()
-        return False
+            u = queue.pop(0)
 
-    def get_AugmentingPath(self, source, sink):
-        path = []
-        while sink != source:
-            edge = sink.parent  # Use the parent attribute of Vertex
-            path.insert(0, edge)
-            sink = edge.start_vertex
-        return path
+            for edge in u.edges:
+                v = edge.target
+                if (not visited[self.vertices.index(v)]) and (edge.capacity - edge.flow > 0):
+                    queue.append(v)
+                    visited[self.vertices.index(v)] = True
+                    parent[self.vertices.index(v)] = u
 
-    def _reset_visited(self):
-        for vertex in self.vertices:
-            vertex.visited = False
-            #vertex.parent = None
+        return visited[self.vertices.index(sink)]
 
-    def augment_flow(self, path):
-        # This method should update the flow in the residual network based on the augmenting path
-        min_residual_capacity = min(edge.capacity - edge.flow for edge in path)
-        for edge in path:
-            edge.flow += min_residual_capacity
+    def ford_fulkerson(self, source, sink):
+        parent = [-1] * len(self.vertices)
+        max_flow = 0
 
-    def ford_fulkerson(self):
-        flow=0
-        while self.has_AugmentingPath(self.vertices[0], self.vertices[-1]):
-            path= self.get_AugmentingPath(self.vertices[0], self.vertices[-1])
-            min_residual_capacity = min(edge.capacity - edge.flow for edge in path)
-            flow += min_residual_capacity
-            self.augment_flow(path)
-        return flow
+        while self.bfs(source, sink, parent):
+            path_flow = float("inf")
+            s = sink
 
-    def get_connected_vertices(self):
-        connected_vertices = []
-        d_vertices = [vertex for vertex in self.vertices if vertex.name.startswith('d')]
-        c_vertices = [vertex for vertex in self.vertices if vertex.name.startswith('c')]
+            while s != source:
+                u = parent[self.vertices.index(s)]
+                for edge in u.edges:
+                    if edge.target == s:
+                        path_flow = min(path_flow, edge.capacity - edge.flow)
+                        break
+                s = u
 
-        for d_vertex, c_vertex in zip(d_vertices, c_vertices):
-            p_vertices_d = [edge.start_vertex.name for edge in self.edges if edge.end_vertex == d_vertex and edge.flow > 0]
-            p_vertices_c = [edge.start_vertex.name for edge in self.edges if edge.end_vertex == c_vertex and edge.flow > 0]
-            connected_vertices.append([p_vertices_d, p_vertices_c])
-        connected_vertices= [sum(sublist, []) for sublist in connected_vertices]
-        return connected_vertices
+            max_flow += path_flow
+            v = sink
 
-if __name__ == '__main__':
-    preferences = [[0, 2], [1], [1, 0], [0], [0], [1], [0], [2], [2], [2], [2, 1], [1]]
-    licences = [0, 3, 2, 5, 10, 9]
-    # Create vertices
-    source1 = Vertex('source1')
-    source2 = Vertex('source2')
-    sink = Vertex('sink')
-    len_pref=len(preferences)
-    p_vertices = [Vertex(f'p{i}') for i in range(len_pref)]
-    d_vertices = [Vertex(f'd{i}') for i in range(math.ceil(len_pref/5))]
-    c_vertices = [Vertex(f'c{i}') for i in range(math.ceil(len_pref/5))]
+            while v != source:
+                u = parent[self.vertices.index(v)]
+                for edge in u.edges:
+                    if edge.target == v:
+                        edge.flow += path_flow
+                        break
+                v = u
 
-    # Create network flow
-    my_graph = NetworkFlow()
-    for vertex in [source1, source2] + p_vertices + d_vertices + c_vertices + [sink]:
-        my_graph.add_vertex(vertex)
+        return max_flow
 
-    # Add edge from source1 to source2
-    my_graph.add_edge(Edge(source1, source2, len_pref))
+    def __str__(self):
+        return super().__str__()
 
-    # Add edges from source2 to p_vertices
-    for p_vertex in p_vertices:
-        my_graph.add_edge(Edge(source2, p_vertex, 1))
+    def make_network(self, preferences, licenses):
+        # Create source and sink vertices
+        source = self.add_vertex("source")
+        sink = self.add_vertex("sink")
 
-    # Add edges from p_vertices to d_vertices
-    edges_from_p_to_d = {
-        'p0': ['d0','d2'],
-        'p3': ['d0'],
-        'p2': ['d0','d1'],
-        'p5': ['d1'],
-        'p10':['d2','d1'],
-        'p9': ['d2']
-    }
-    for p_name, d_names in edges_from_p_to_d.items():
-        p_vertex = next(vertex for vertex in p_vertices if vertex.name == p_name)
-        for d_name in d_names:
-            d_vertex = next(vertex for vertex in d_vertices if vertex.name == d_name)
-            my_graph.add_edge(Edge(p_vertex, d_vertex, 1))
+        # Create p vertices and connect them to source
+        p_vertices = [self.add_vertex(f"p{i}") for i in range(len(preferences))]
+        for p_vertex in p_vertices:
+            self.add_edge(source, p_vertex, 1)
 
-    # Add edges from d_vertices to sink
-    for d_vertex in d_vertices:
-        my_graph.add_edge(Edge(d_vertex, sink, 2))
+        # Create d vertices
+        num_d_vertices = math.ceil(len(preferences) / 5)
+        d_vertices = [self.add_vertex(f"d{i}") for i in range(num_d_vertices)]
 
-    # Add edges from p_vertices to c0
-    for p_vertex in [p_vertices[i] for i in [0,2,3,4,6]]:
-        my_graph.add_edge(Edge(p_vertex, c_vertices[0], 1))
+        # Create c vertices
+        num_c_vertices = math.ceil(len(preferences) / 5)
+        c_vertices = [self.add_vertex(f"c{i}") for i in range(num_c_vertices)]
 
-    # Add edges from p_vertices to c1
-    for p_vertex in [p_vertices[i] for i in [1,2,5,10,11]]:
-        my_graph.add_edge(Edge(p_vertex, c_vertices[1], 1))
+        # Connect p vertices with d and c vertices based on preferences
+        for i, p_vertex in enumerate(p_vertices):
+            for pref in preferences[i]:
+                # Connect to c vertices
+                self.add_edge(p_vertex, c_vertices[pref], 1)
+                # Connect licensed p vertices to d vertices
+                if i in licenses:
+                    self.add_edge(p_vertex, d_vertices[pref], 1)
 
-    # Add edges from p_vertices to c2
-    for p_vertex in [p_vertices[i] for i in [0, 7,8,9, 10]]:
-        my_graph.add_edge(Edge(p_vertex, c_vertices[2], 1))
+        # Connect d vertices to sink with capacity 2
+        for d_vertex in d_vertices:
+            self.add_edge(d_vertex, sink, 2)
 
-    # Add edges from c_vertices to sink
-    for c_vertex in c_vertices:
-        my_graph.add_edge(Edge(c_vertex, sink, 3))
-
-    # Run Ford-Fulkerson algorithm
-    residual=ResidualNetwork(my_graph)
-    residual.ford_fulkerson()
-    print(residual)
-    print(residual.get_connected_vertices())
+        # Connect c vertices to sink with capacity 3
+        for c_vertex in c_vertices:
+            self.add_edge(c_vertex, sink, 3)
 
 
-
+# Test the classes
+network = ResidualNetwork()
+v1 = network.add_vertex("A")
+v2 = network.add_vertex("B")
+v3 = network.add_vertex("C")
+network.add_edge(v1, v2, 10)
+network.add_edge(v1, v3, 5)
+network.add_edge(v2, v3, 15)
+print(network)
+network.ford_fulkerson(v1,v3)
+print(network)
